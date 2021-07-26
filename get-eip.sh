8f7521116e5a7303b64b5141128203e827bfda2a
Paweł Kosiec <pawel@capact.io>
2021-07-26 13:56:59 +0200

    Install kubeconfig if not installed

--- get-eip.sh
+++ get-eip.sh
@@ -2,6 +2,23 @@
 
 export KUBECONFIG=${1:-~/.kube/config}
 
+# Install Kubectl if not installed
+if ! [ -x "$(command -v kubectl)" ]; then
+  set -e
+
+  OS=$(uname -s)
+  if [ "$OS" != "Linux" ]; then
+    echo "The kubectl binary is required. Please install it manually"
+    exit 1;
+  fi
+
+  wget https://dl.k8s.io/release/v1.19.13/bin/linux/amd64/kubectl 1>/dev/null
+  chmod +x ./kubectl
+  mv kubectl /usr/local/bin
+  kubectl version 1>/dev/null
+  set +e
+fi
+
 # Wait for the istio namesapce so get svc doesn't puke
 until kubectl get ns istio-system 1>&- 2>&-
 do
